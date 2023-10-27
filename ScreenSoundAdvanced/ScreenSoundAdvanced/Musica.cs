f6cfb6ae6d95720fe2832e7ab316c1c38fcec635
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2023-10-27 18:11:16 -0300

    Create new project: ScreenSoundAdvanced to learn object orientation

---
+++ ScreenSoundAdvanced/ScreenSoundAdvanced/Musica.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Channels;
+
+class Musica
+{
+    public string nome, artista;
+    public int duracao;
+    public bool disponivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disponivel)
+        {
+            Console.WriteLine("Disponível no plano\n");
+        }
+        else
+        {
+            Console.WriteLine("Adquira o plano Plus+\n");
+        }
+    }
+}
---
+++ ScreenSoundAdvanced/ScreenSoundAdvanced/Program.cs
@@ -0,0 +1,14 @@
+﻿Musica musica1 = new Musica();
+musica1.nome = "Ressucita-me";
+musica1.artista = "Aline Barros";
+musica1.duracao = 289;
+musica1.disponivel = true;
+
+Musica musica2 = new Musica();
+musica2.nome = "Meu Barquinho";
+musica2.artista = "Giselli Cristina";
+musica2.duracao = 322;
+musica2.disponivel = false;
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
